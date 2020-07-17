ea60062fbde7e48d79e72a50d41670cd643c7c9e
mly <mly@ongconseil.com>
2020-07-17 10:52:14 +0200

    Separate footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,21 @@
-
+//Get element with Jquery
 $(function(){
   $("#sidebar").load("sidebar.html"); 
 });
 
+//Get elment with Jquery + moustache
 $.get('header.html', function(templates) {
     // Fetch the <script /> block from the loaded external
     // template file which contains our greetings template.
-    console.log("test")
     var header = $(templates).filter('#tpl-header').html();
     let headerData = {};
     $('.page-wrapper').append(Mustache.render(header, headerData));
+});
+
+$.get('footer.html', function(templates) {
+    // Fetch the <script /> block from the loaded external
+    // template file which contains our greetings template.
+    var header = $(templates).filter('#tpl-footer').html();
+    let headerData = {};
+    $('#footer').append(Mustache.render(header, headerData));
 });
