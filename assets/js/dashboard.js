920514a6acb1bdf6a39a1c01c6cda81b728c1213
mly <mly@ongconseil.com>
2020-07-28 10:07:57 +0200

    -Reorganize the files (could have some error of redirection) -Trying to do the auth with api

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,23 +1,6 @@
 //Get element with Jquery
 $(function(){
   $("#sidebar").load("sidebar.html");
-  console.log("test");
-    var url = window.location.url;
-    let element = document.getElementsByClassName("has-sub");
-    for (let i = 0; i < element.length; i++) {
-        console.log("inside1");
-
-        let navText = element[i].getElementsByTagName('a');
-        for (let j = 0; j < navText.length; j++) {
-            if(navText[j].href == url) {
-              console.log("inside2");
-
-                element[i].classList.toggle('active');
-                let t = element[i].getElementsByTagName('ul');
-                t[0].classList.toggle('show');
-            }
-        }
-    }
 });
 
 //Get elment with Jquery + moustache
