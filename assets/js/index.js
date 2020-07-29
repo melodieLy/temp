4e21394e357d35db388a939905bf06559da351e1
mly <mly@ongconseil.com>
2020-07-29 11:53:10 +0200

    up

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+function Auth(theForm) {
+    $.ajax({
+      url: "https://recette-api.song-fr.com/swatoken",
+      headers: {
+        'content-Type': "application/x-www-form-urlencoded"
+      },
+      method: "POST",
+      withCredentials: true,
+      data : {
+        grant_type:"password",            
+        username: theForm.email.value,
+        password: theForm.password.value,
+      }
+    })
+      .done(function(result) {
+        const time = 1000;
+        let expire = new Date();
+        date.setTime(date.getTime()+ (result.expires_in * time));
+        createCookie(result,date);
+      })
+      .fail(function(xhr, status, error) {
+        console.log("status : "+ xhr.status);
+        console.log("message : "+ xhr.statusText);
+      })
+}
+
+function createCookie(setup, time) {
+    document.cookie = "token=" + setup.access_token + ";expires=" + now.setTime(time) + ";token_type="+ setup.token_type;
+}
