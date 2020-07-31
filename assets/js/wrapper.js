73ba8503e4e52309bc59b586c7a8e7de28c52059
mly <mly@ongconseil.com>
2020-07-31 12:40:49 +0200

    up

--- assets/js/wrapper.js
+++ assets/js/wrapper.js
@@ -5,7 +5,7 @@ function getCookie() {
     let array = document.cookie.split(";");
     let result = new Array();
     result.expires = (array[0].split('=').pop());
-    result.token = (array[1].split('=').pop());
+    result.token = "bearer " + (array[1].split('=').pop());
     return result;
 };
 
@@ -16,7 +16,7 @@ function hello() {
 const cookies = getCookie();
 $(document).ready(function() {
         if(cookies === undefined) {
-        window.location.replace("index.html");
+        // window.location.replace("index.html");
         alert("cookies not found");
 
     } else if (cookies.expires < $.now()) {
@@ -33,7 +33,7 @@ function get(path) {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept":"application/json",
-            "Authorization": + "bearer " + cookies.token
+            "Authorization": "bearer z_hp-2hlYHiTVGd2RiL9C1Aic76hEabnjnaBgeXaYQRlK0tBDJx-qbRJ2BmmKT93oeOkmjTP75dFFyKSbRxPtFtvNHP9zxCQF1FFNtQ0oeXhwOlRkmlDtbCoAV-okBFUzYz8zmm7TvgBcwt0LFRxDUDqeAksg_yVzgf8UpGW2_WrAK9rflWutqaojx-zo58F_hgwescpSdZjdFrOgAsbYJ6U3dnkVZsj0vlCRrWpN1vNpKqeLGYW0xWFQ_85pIBBfHBJ5hhkAFec1wHMSCtkQAs0HGq7Lw2PWCrYFpQfvSMhqjWh4Z4jwRVT7gLFg9qPuH1VAJ3mSASN496xfY4R1G7I8XW7-kGV_6ngYdiaKW0Ol-o9t95NFz0sV0j926W9NQ3uXjyZCD7s6n3c83hNcw" 
         },
         method: "GET",
     })
