7423023854d07a241e3baac13189ff6193b73e7f
mly <mly@ongconseil.com>
2020-08-04 15:07:18 +0200

    up

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,9 @@
 get("public/associations/"+cookies.assoId+"/logo", retrieveAssoLogo);
 
+$(function(){
+    $("#association").append(Mustache.render('<span class="brand-name text-truncate">{{Name}}</span>',{name: cookies.asso}))
+  });
+
 function retrieveAssoLogo(data) {
     $.get('components/logo.html', function(templates) {
         var component = $(templates).filter('#association').html();
