cca6e3e527be57d6974ab3a41c62a3b94dfb075a
mly <mly@ongconseil.com>
2020-08-05 15:26:02 +0200

    up

--- assets/js/data_table.js
+++ assets/js/data_table.js
@@ -1,6 +1,6 @@
 $.get('components/members_table.html', function(templates) {
     var component = $(templates).filter('#tpl-members-table').html();
-    $.getJSON("/assets/js/data_table.json", function (data){
+    $.getJSON("assets/js/data_table.json", function (data){
         console.log(data);
         // $('#members').append(Mustache.render(component,data));
     })
