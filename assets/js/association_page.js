360297f6a66d96b459d8a4dbc30589b28e738fd0
mly <mly@ongconseil.com>
2020-08-11 14:38:08 +0200

    up

--- assets/js/association_page.js
+++ assets/js/association_page.js
@@ -1,4 +1,4 @@
-$.get('components/flux_principal.html', function(templates) {
+$.get('components/flux-principal.html', function(templates) {
     var component = $(templates).filter('#tpl-flux-pri').html();
     const data = {}
     $('#flux').append(Mustache.render(component,data));
