0486994b1d205b75e354ef8cbd63e547c6d9b190
mly <mly@ongconseil.com>
2020-08-14 11:24:44 +0200

    up

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,12 +1,12 @@
-get("public/associations/"+cookies.assoId+"/logo", retrieveAssoLogo);
+// get("public/associations/"+cookies.assoId+"/logo", retrieveAssoLogo);
 
 $(function(){
     $("#association").append(Mustache.render('<span class="brand-name text-truncate">{{Name}}</span>',{Name: cookies.asso}))
   });
 
-function retrieveAssoLogo(data) {
-    $.get('components/logo.html', function(templates) {
-        var component = $(templates).filter('#association').html();
-        $('#logo').append(Mustache.render(component,{img_src:data}));
-    });
-}
+// function retrieveAssoLogo(data) {
+//     $.get('components/logo.html', function(templates) {
+//         var component = $(templates).filter('#association').html();
+//         $('#logo').append(Mustache.render(component,{img_src:data}));
+//     });
+// }
