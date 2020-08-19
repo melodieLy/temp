622e5fff8ae0f568cc22b6dd22f018ea2bf7dc94
mly <mly@ongconseil.com>
2020-08-19 14:22:49 +0200

    Add mailjet_page.js need to finish

---
+++ assets/js/mailjet_page.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+    jQuery('#basic-data-table').DataTable({
+     "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+    });
+   });
