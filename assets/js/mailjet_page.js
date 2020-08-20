70bc74d7f4f9288fc65ed5d48c4f645f75ad41f5
mly <mly@ongconseil.com>
2020-08-20 14:24:37 +0200

    up

--- assets/js/mailjet_page.js
+++ assets/js/mailjet_page.js
@@ -6,5 +6,6 @@ $.get('components/mailjet-table.html', function(templates) {
         $('#basic-data-table').DataTable({
             "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
             });
+        document.getElementById("basic-data-table_wrapper").style.width = "100%";
     })
 });
