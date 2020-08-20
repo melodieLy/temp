5483522dafb3eb18768e307b2485b79ba41fc804
mly <mly@ongconseil.com>
2020-08-20 14:07:13 +0200

    up

--- assets/js/mailjet_page.js
+++ assets/js/mailjet_page.js
@@ -1,9 +1,10 @@
- 
 
 $.get('components/mailjet-table.html', function(templates) {
     var component = $(templates).filter('#tpl-mailjet-table').html();
     $.getJSON("assets/js/data_mailjet.json", function (data){
-        console.log("o");
-    $('#mailjet-table').append(Mustache.render(component,data));
+        $('#mailjet-table').append(Mustache.render(component,data));
+        $('#basic-data-table').DataTable({
+            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+            });
     })
 });
