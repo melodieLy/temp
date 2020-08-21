8a8598346257a7cd9e05ec1cc8811e1ff7e059ec
mly <mly@ongconseil.com>
2020-08-21 10:39:54 +0200

    up

--- assets/js/association_page.js
+++ assets/js/association_page.js
@@ -14,10 +14,8 @@ $.get('components/FTP-serv-model.html', function(templates) {
 $.get('components/flux-histo-model.html', function(templates) {
     var component = $(templates).filter('#tpl-flux-histo').html();
     const data = {}
-    $('#serv').append(Mustache.render(component,data));
+    $('#histo').append(Mustache.render(component,data));
     $('#data-table').DataTable({
-        "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
-        "pageLength": 20,
         "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
     });
 });
--- assets/js/data_table.js
+++ assets/js/data_table.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
 $.get('components/members_table.html', function(templates) {
     var component = $(templates).filter('#tpl-members-table').html();
     $.getJSON("assets/js/data_table.json", function (data){
-        console.log(data);
         $('#members').append(Mustache.render(component,data));
     })
 });
