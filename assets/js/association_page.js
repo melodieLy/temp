295d7c7d5ee8db9445f65a14ced114f68aebb4ad
mly <mly@ongconseil.com>
2020-09-01 16:39:14 +0200

    up

--- assets/js/association_page.js
+++ assets/js/association_page.js
@@ -5,14 +5,16 @@ function retrieveFlows(data) {
         var component = $(templates).filter('#tpl-flux-pri').html();
         $('#flux').append(Mustache.render(component,data));
     });
+
+    $.get('components/FTP-serv-model.html', function(templates) {
+        var component = $(templates).filter('#tpl-ftp-serv').html();
+        console.log(data);
+        $('#serv').append(Mustache.render(component,data));
+    });
 }
 
 
-$.get('components/FTP-serv-model.html', function(templates) {
-    var component = $(templates).filter('#tpl-ftp-serv').html();
-    const data = {}
-    $('#serv').append(Mustache.render(component,data));
-});
+
 
 
 $.get('components/flux-histo-model.html', function(templates) {
