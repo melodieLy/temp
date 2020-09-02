ad1bb8f7280ad108e77253bfa790c1f986509ec0
mly <mly@ongconseil.com>
2020-09-02 15:40:31 +0200

    up

--- assets/js/data_table.js
+++ assets/js/data_table.js
@@ -1,14 +1,19 @@
-$(document).ready(function() {
-    jQuery('#hoverable-data-table').DataTable({
-     "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
-     "pageLength": 20,
-     "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-    });
-   });
+// $(document).ready(function() {
+//     jQuery('#hoverable-data-table').DataTable({
+//      "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
+//      "pageLength": 20,
+//      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+//     });
+//    });
 
 $.get('components/members_table.html', function(templates) {
     var component = $(templates).filter('#tpl-members-table').html();
     $.getJSON("assets/js/data_table.json", function (data){
         $('#test').append(Mustache.render(component,data));
     })
+    $('#hoverable-data-table').DataTable({
+        "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
+        "pageLength": 20,
+        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+    });
 });
