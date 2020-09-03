c0ec4aa3b3eb9012aa3256da776c00272e8f1d1a
mly <mly@ongconseil.com>
2020-09-03 11:37:57 +0200

    up

--- assets/js/data_table.js
+++ assets/js/data_table.js
@@ -13,6 +13,7 @@ $.get('components/members_table.html', function(templates) {
         $('#basic-data-table').DataTable({
             "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
         });
+        document.getElementById("basic-data-table_wrapper").style.width = "100%";
     })
    
 });
