bb2d76b10a74ab1f45aefc159f2d927d0218e27e
mly <mly@ongconseil.com>
2020-09-29 12:34:43 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -4,14 +4,15 @@ function retrieveContacts(data) {
     $.get('components/contacts_table.html', function(templates) {
         var component = $(templates).filter('#tpl-contacts-table').html();
         $('#contacts').append(Mustache.render(component,data));
-        $(document).ready(function() {
-            $('#basic-data-table').DataTable({
-                "aLengthMenu": [[10, 30, 50, 75, -1], [10, 30, 50, 75, "All"]],
-                "pageLength": 10,
-                "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-            });
-            document.getElementById("basic-data-table_wrapper").style.width = "100%";
+    });
+
+    $(document).ready(function() {
+        $('#basic-data-table').DataTable({
+            "aLengthMenu": [[10, 30, 50, 75, -1], [10, 30, 50, 75, "All"]],
+            "pageLength": 10,
+            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
         });
+        document.getElementById("basic-data-table_wrapper").style.width = "100%";
     });
 };
 // $(document).ready(function() {
