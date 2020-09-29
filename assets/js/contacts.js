0f34e555badee869b8e956774f4c4e16678bee3c
mly <mly@ongconseil.com>
2020-09-29 13:35:53 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -5,20 +5,13 @@ function retrieveContacts(data) {
         var component = $(templates).filter('#tpl-contacts-table').html();
         $('#contacts').append(Mustache.render(component,data));
     });
-
-    $(document).ready(function() {
-        $('#basic-data-table').DataTable({
-            "aLengthMenu": [[10, 30, 50, 75, -1], [10, 30, 50, 75, "All"]],
-            "pageLength": 10,
-            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-        });
-        document.getElementById("basic-data-table_wrapper").style.width = "100%";
-    });
 };
-// $(document).ready(function() {
-//     jQuery('#hoverable-data-table').DataTable({
-//      "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
-//      "pageLength": 20,
-//      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
-//     });
-//    });
+
+$(document).ready(function() {
+    $('#basic-data-table').DataTable({
+        "aLengthMenu": [[10, 30, 50, 75, -1], [10, 30, 50, 75, "All"]],
+        "pageLength": 10,
+        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+    });
+    document.getElementById("basic-data-table_wrapper").style.width = "100%";
+});
