4d4f02ece11424743d882c8590ee589139413ac6
mly <mly@ongconseil.com>
2020-09-29 11:13:00 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -10,8 +10,8 @@ function retrieveContacts(data) {
             "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
         });
         document.getElementById("basic-data-table_wrapper").style.width = "100%";
+    });
 };
-
 // $(document).ready(function() {
 //     jQuery('#hoverable-data-table').DataTable({
 //      "aLengthMenu": [[20, 30, 50, 75, -1], [20, 30, 50, 75, "All"]],
