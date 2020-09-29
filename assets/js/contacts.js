4149b4c22f74711aa2b24e1d24009a2b60a00ea9
mly <mly@ongconseil.com>
2020-09-29 15:13:54 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,17 +1,27 @@
-get('associations/'+cookies.assoId + '/contacts', retrieveContacts);
+get(, retrieveContacts);
 
 function retrieveContacts(data) {
-    $.get('components/contacts_table.html', function(templates) {
-        var component = $(templates).filter('#tpl-contacts-table').html();
-        $('#contacts').append(Mustache.render(component,data));
         $(document).ready(function() {
             $('#basic-data-table').DataTable({
                 // "aLengthMenu": [[10, 30, 50, 75, -1], [10, 30, 50, 75, "All"]],
                 // "pageLength": 10,
                 "processing": true,
                 "serverSide": true,
+                "ajax": {
+                    url: "https://recette-api.song-fr.com/"+'associations/'+cookies.assoId + '/contacts',
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Accept":"application/json",
+                        "Authorization": cookies.token
+                    },
+                    method: "GET"
+                },
                 "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
             });
+            // $.get('components/contacts_table.html', function(templates) {
+            //     var component = $(templates).filter('#tpl-contacts-table').html();
+            //     $('#contacts').append(Mustache.render(component,data)); 
+            // });
             document.getElementById("basic-data-table_wrapper").style.width = "100%";
         });
     });
