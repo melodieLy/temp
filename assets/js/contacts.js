62fbe630af0cabab7b89faf85ecaa74a88594910
mly <mly@ongconseil.com>
2020-09-29 16:07:37 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -15,9 +15,10 @@
                     },
                     method: "GET",
                     "data": function ( d ) {
-                        console.log("att");
-                        var component = $(templates).filter('#tpl-contacts-table').html();
-                        $('#contacts').append(Mustache.render(component,d)); 
+                        $.get('components/contacts_table.html', function(templates) {
+                            var component = $(templates).filter('#tpl-contacts-table').html();
+                            $('#contacts').append(Mustache.render(component,d)); 
+                        });
                     }
                 },
                 "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
