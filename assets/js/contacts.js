621bd14a5c26cd45281f1e308571e1e1f60b836f
mly <mly@ongconseil.com>
2020-10-08 15:40:26 +0200

    up

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -5,9 +5,9 @@ function retrieveContacts(data) {
     $.get('components/contacts_table.html', function(templates) {
         var component = $(templates).filter('#tpl-contacts-table').html();
         $('#contacts').append(Mustache.render(component,data));
-        $('#basic-contacts-table').show()
         $('#basic-contacts-table').DataTable({
-            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
+            "initComplete": () => {$("#basic-contacts-table").show();}
           });
     });
 };
