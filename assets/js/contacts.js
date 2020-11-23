71e9bc84a1caee84b540f7575a22cfb90f0b543a
mly <mly@ongconseil.com>
2020-11-23 11:26:37 +0100

    New test using the AJAX by Datatable

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,15 +1,29 @@
 
-get('associations/'+cookies.assoId + '/contacts',retrieveContacts);
+// get('associations/'+cookies.assoId + '/contacts',retrieveContacts);
 
-function retrieveContacts(data) {
-    $.get('components/contacts_table.html', function(templates) {
-        var component = $(templates).filter('#tpl-contacts-table').html();
-        $('#contacts').append(Mustache.render(component,data));
-        $('#basic-contacts-table').DataTable({
-            "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
-            "initComplete": () => {$("#basic-contacts-table").show();},
-            "deferLoading": 100
-          });
-    });
-};
+// function retrieveContacts(data) {
+//     $.get('components/contacts_table.html', function(templates) {
+//         var component = $(templates).filter('#tpl-contacts-table').html();
+//         $('#contacts').append(Mustache.render(component,data));
+//         $('#basic-contacts-table').DataTable({
+//             "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
+//             "initComplete": () => {$("#basic-contacts-table").show();}
+//           });
+//     });
+// };
+
+$document.ready(function(){
+    $('#myTable').DataTable({
+        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
+        "ajax": {
+            "url": "https://recette-api.song-fr.com/associations/Aides/contacts",
+            "type": "GET",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept":"application/json",
+                "Authorization": cookies.token
+            }
+        }
+    })
+})
 
