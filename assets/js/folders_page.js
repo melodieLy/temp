b4f1a593ae272bfebb963313a9c18fe3e5c417ca
mly <mly@ongconseil.com>
2020-11-26 10:15:10 +0100

    corrected name

--- assets/js/folders_page.js
+++ assets/js/folders_page.js
@@ -3,7 +3,7 @@ get('folders/'+cookies.assoId, getAccessibleFolders);
 function getAccessibleFolders(data) {
   console.log(data)
   $.get('folders_table.html', function(templates) {
-    var component = $(templates).filter('#basic-folders-table').html();
+    var component = $(templates).filter('#tpl-folders-table').html();
     $('#folders').append(Mustache.render(component,data));
   })
 }
