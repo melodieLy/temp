6056452f1ec3f45ce7cc268ee6e55494888a9a62
mly <mly@ongconseil.com>
2020-11-26 11:11:29 +0100

    added missing path

--- assets/js/folders_page.js
+++ assets/js/folders_page.js
@@ -2,7 +2,7 @@ get('folders/'+cookies.assoId, getAccessibleFolders);
 
 function getAccessibleFolders(data) {
   console.log(data)
-  $.get('folders_table.html', function(templates) {
+  $.get('components/folders_table.html', function(templates) {
     var component = $(templates).filter('#tpl-folders-table').html();
     $('#folders').append(Mustache.render(component,data));
     $('#basic-folders-table').DataTable({
