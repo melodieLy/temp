6d64fdb0b663b22b5af4445e9c43773417c80bee
mly <mly@ongconseil.com>
2020-11-26 11:00:29 +0100

    up

--- assets/js/folders_page.js
+++ assets/js/folders_page.js
@@ -6,8 +6,8 @@ function getAccessibleFolders(data) {
     var component = $(templates).filter('#tpl-folders-table').html();
     $('#folders').append(Mustache.render(component,data));
     $('#basic-folders-table').DataTable({
-        "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
-        "initComplete": () => {$("#basic-folders-table").show();}
+        // "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
+        // "initComplete": () => {$("#basic-folders-table").show();}
       });
   })
 }
