c4c77d1b52d914fa02979210786e6fd1815d63d9
mly <mly@ongconseil.com>
2020-12-02 13:30:59 +0100

    up

--- assets/js/folders_page.js
+++ assets/js/folders_page.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
-  let path = window.location.search;
+  var path = window.location.search;
 
   if (path != "") {
     path = path.replace('?id=', '');
-    // get('folder/'+cookies.assoId+'/'+path)
-    console.log(path)
+    get('folder/'+cookies.assoId+'/'+path,getAccesibleFiles);
   } 
   else {
     get('folders/'+cookies.assoId, getAccessibleFolders);
   }
 });
 
-var test;
-
 function getAccessibleFolders(data) {
-  test = data;
+  $.get('components/files_table.html', function(templates) {
+    var component = $(templates).filter('#tpl-folders-table').html();
+    $('#folders').append(Mustache.render(component,data));
+  })
+}
+
+function getAccesibleFiles(data) {
   $.get('components/folders_table.html', function(templates) {
     var component = $(templates).filter('#tpl-folders-table').html();
-    $('#folders').append(Mustache.render(component,test));
+    data.forEach(element => {
+      if(element.MimeType === "image/png") console.log("img")
+      else if (element.MimeType === "text/plain") console.log("txt")
+    });
+    $('#folders').append(Mustache.render(component,data));
   })
 }
