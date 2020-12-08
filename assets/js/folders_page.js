c34c7992f8a5fab3d2c39af8eb3521b776a8a8d9
mly <mly@ongconseil.com>
2020-12-08 10:50:01 +0100

    up

--- assets/js/folders_page.js
+++ assets/js/folders_page.js
@@ -2,12 +2,10 @@ var path = window.location.search;
 $(document).ready(function() {
 
   if (path != "") {
-    console.log("somethong")
     path = path.replace('?id=', '');
     get('folders/'+cookies.assoId+'/'+ path,getAccesibleFiles);
   } 
   else {
-    console.log("nonthing")
     get('folders/'+cookies.assoId, getAccessibleFolders);
   }
 });
@@ -35,34 +33,6 @@ function getAccesibleFiles(data) {
     });
     $('#folders').append(Mustache.render(component,data))
   })
-}
-
-function test (data) {
-  const jsonBlob = new Blob([data])
-  console.log(jsonBlob)
-  const blobUrl = window.URL.createObjectURL(jsonBlob);
-    //Create a link element
-  const link = document.createElement("a");
-
-  //Set link's href to point to the blob URL
-  link.href = blobUrl;
-  link.download = document.getElementById("");
-
-  //Append link tot he body
-  document.body.appendChild(link);
-
-  //Dispatch click event ont he link
-  // This is necessary as link.click() does not work on the latest firefox
-  link.dispatchEvent(
-    new MouseEvent('click', { 
-      bubbles: true, 
-      cancelable: true, 
-      view: window 
-    })
-  );
-  
-  // Remove link from body
-  document.body.removeChild(link);
 }
 
 function downloadBlob(blob, name) {
