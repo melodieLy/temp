9658ea0b3ee4e2c669f4507cbdc1ba7ce6339053
mly <mly@ongconseil.com>
2021-02-02 15:43:26 +0100

    finish logo, update size

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,5 @@
 $(function(){
-  $("#association").append(Mustache.render('<img src="{{img_src}}" class="brand-icon" width="30" id="logo" /><span class="brand-name text-truncate">{{Name}}</span>',{
+  $("#association").append(Mustache.render('<img src="{{img_src}}" class="brand-icon" width="70" id="logo" /><span class="brand-name text-truncate">{{Name}}</span>',{
     img_src:retrieveAssoLogo(),
     Name: cookies.asso
   }))
