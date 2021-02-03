9e2bb00cd84d3e2c267ef3915d5bf17be84f777d
mly <mly@ongconseil.com>
2021-02-03 15:18:48 +0100

    add the page number on the request. testing the hash effect

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -1,10 +1,14 @@
-
-get('calls/called/'+cookies.assoId , retrieveWelcomeCalls);
+get('calls/called/'+cookies.assoId+'?Page=1' , retrieveWelcomeCalls);
 
 function retrieveWelcomeCalls(data) {
     $.get('components/wc_table.html', function(templates) {
         var component = $(templates).filter('#tpl-wc-table').html();
         $('#welcome-call').append(Mustache.render(component,data));
+
+        var paginationSetup = JSON.parse(pm.response.headers.get("X-Pagination"));
+        const totalPage = paginationSetup.totalPage;
+        pm.environment.set("nextPageLink", nextPageLink);
+
         // $('#basic-wc-table').DataTable({
         //     "pageLength":20,
         //     "serverSide": true,
