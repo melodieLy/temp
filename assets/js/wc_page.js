61fe8bad3c6b03800efc6fed6a2b5803224321d3
mly <mly@ongconseil.com>
2021-02-03 17:53:21 +0100

    testing

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -1,37 +1,82 @@
-get('calls/called/'+cookies.assoId+'?Page=1' , retrieveWelcomeCalls);
+// get('calls/called/'+cookies.assoId+'?Page=1' , retrieveWelcomeCalls);
 
-function retrieveWelcomeCalls(data, Status,XMLHttpRequest) {
-    $.get('components/wc_table.html', function(templates) {
-        var component = $(templates).filter('#tpl-wc-table').html();
-        $('#welcome-call').append(Mustache.render(component,data));
+$.ajax({
+    url: "https://recette-api.song-fr.com/calls/called/"+cookies.assoId+'?Page=1',
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept":"application/json",
+        "Authorization": cookies.token
+    },
+    method: "GET",
+    success: function (data, textStatus, request) {
+        $.get('components/wc_table.html', function(templates) {
+            var component = $(templates).filter('#tpl-wc-table').html();
+            $('#welcome-call').append(Mustache.render(component,data));
+        });
 
-        // $('#basic-wc-table').DataTable({
-        //     "pageLength":20,
-        //     "serverSide": true,
-        //     "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
-        //     "initComplete": () => {$("#basic-wc-table").show();
-        //     }
-        //   });
-    });
+        $.get('components/pagination.html', function(templates) {
+            var component = $(templates).filter('#pagination-comp').html();
+            
+            const paginationSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
+    
+            console.log(paginationSetup);
+            const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
+            paginationSetup.totalArrayPage = totalsArrayPage;
+            paginationSetup.previousPage = function () {
+                const result = this.PageNumber - 1;
+                if(result == 0) return undefined;
+                else return result;
+            }
+    
+            paginationSetup.nextPage = function () {
+                const result = this.PageNumber - 1;
+                if(result > this.totalPage) return undefined;
+                else return result;
+            }
+    
+            $('#welcome-call').append(Mustache.render(component,paginationSetup));
+        });
+    }
+})
 
-    $.get('components/pagination.html', function(templates) {
-        var component = $(templates).filter('#pagination-comp').html();
+.fail(function(xhr,textStatus, errorThrown) {
+    getError(xhr,textStatus, errorThrown);
+});
+
+// function retrieveWelcomeCalls(data, Status,XMLHttpRequest) {
+//     $.get('components/wc_table.html', function(templates) {
+//         var component = $(templates).filter('#tpl-wc-table').html();
+//         $('#welcome-call').append(Mustache.render(component,data));
+
+//         // $('#basic-wc-table').DataTable({
+//         //     "pageLength":20,
+//         //     "serverSide": true,
+//         //     "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
+//         //     "initComplete": () => {$("#basic-wc-table").show();
+//         //     }
+//         //   });
+//     });
+
+    // $.get('components/pagination.html', function(templates) {
+    //     var component = $(templates).filter('#pagination-comp').html();
         
-        const paginationSetup = JSON.parse(XMLHttpRequest.getResponseHeader("X-Pagination"));
-        const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
-        paginationSetup.totalArrayPage = totalsArrayPage;
-        paginationSetup.previousPage = function () {
-            const result = this.PageNumber - 1;
-            if(result == 0) return undefined;
-            else return result;
-        }
+    //     const paginationSetup = JSON.parse(XMLHttpRequest.getResponseHeader("X-Pagination"));
 
-        paginationSetup.nextPage = function () {
-            const result = this.PageNumber - 1;
-            if(result > this.totalPage) return undefined;
-            else return result;
-        }
+    //     console.log(paginationSetup);
+    //     const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
+    //     paginationSetup.totalArrayPage = totalsArrayPage;
+    //     paginationSetup.previousPage = function () {
+    //         const result = this.PageNumber - 1;
+    //         if(result == 0) return undefined;
+    //         else return result;
+    //     }
 
-        $('#welcome-call').append(Mustache.render(component,paginationSetup));
-    })
-};
+    //     paginationSetup.nextPage = function () {
+    //         const result = this.PageNumber - 1;
+    //         if(result > this.totalPage) return undefined;
+    //         else return result;
+    //     }
+
+    //     $('#welcome-call').append(Mustache.render(component,paginationSetup));
+//     })
+// };
