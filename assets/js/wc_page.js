8148c14a4ff690730b29c524ab582aa401a8373b
mly <mly@ongconseil.com>
2021-02-03 18:01:59 +0100

    up

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -16,25 +16,25 @@ $.ajax({
 
         $.get('components/pagination.html', function(templates) {
             var component = $(templates).filter('#pagination-comp').html();
-            
-            const paginationSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
+            console.log(request);
+            // const paginationSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
     
-            console.log(paginationSetup);
-            const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
-            paginationSetup.totalArrayPage = totalsArrayPage;
-            paginationSetup.previousPage = function () {
-                const result = this.PageNumber - 1;
-                if(result == 0) return undefined;
-                else return result;
-            }
+            // console.log(paginationSetup);
+            // const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
+            // paginationSetup.totalArrayPage = totalsArrayPage;
+            // paginationSetup.previousPage = function () {
+            //     const result = this.PageNumber - 1;
+            //     if(result == 0) return undefined;
+            //     else return result;
+            // }
     
-            paginationSetup.nextPage = function () {
-                const result = this.PageNumber - 1;
-                if(result > this.totalPage) return undefined;
-                else return result;
-            }
+            // paginationSetup.nextPage = function () {
+            //     const result = this.PageNumber - 1;
+            //     if(result > this.totalPage) return undefined;
+            //     else return result;
+            // }
     
-            $('#welcome-call').append(Mustache.render(component,paginationSetup));
+            // $('#welcome-call').append(Mustache.render(component,paginationSetup));
         });
     }
 })
