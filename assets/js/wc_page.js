2d415bd060cfa6dfbb404d015849b8ea91db7ccb
mly <mly@ongconseil.com>
2021-02-04 16:48:30 +0100

    up

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -17,7 +17,7 @@ $.ajax({
         $.get('components/pagination.html', function(templates) {
             var component = $(templates).filter('#pagination-comp').html();
             console.log(request.getAllResponseHeaders());
-            console.log("pagi: " + request.getResponseHeader("x-pagination"));
+            console.log("pagi: " + request.getResponseHeader("X-Pagination"));
             // const paginationSetup = JSON.parse(request.getResponseHeader("Content-Type"));
     
             // console.log(paginationSetup);
@@ -43,41 +43,3 @@ $.ajax({
 .fail(function(xhr,textStatus, errorThrown) {
     getError(xhr,textStatus, errorThrown);
 });
-
-// function retrieveWelcomeCalls(data, Status,XMLHttpRequest) {
-//     $.get('components/wc_table.html', function(templates) {
-//         var component = $(templates).filter('#tpl-wc-table').html();
-//         $('#welcome-call').append(Mustache.render(component,data));
-
-//         // $('#basic-wc-table').DataTable({
-//         //     "pageLength":20,
-//         //     "serverSide": true,
-//         //     "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
-//         //     "initComplete": () => {$("#basic-wc-table").show();
-//         //     }
-//         //   });
-//     });
-
-    // $.get('components/pagination.html', function(templates) {
-    //     var component = $(templates).filter('#pagination-comp').html();
-        
-    //     const paginationSetup = JSON.parse(XMLHttpRequest.getResponseHeader("X-Pagination"));
-
-    //     console.log(paginationSetup);
-    //     const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
-    //     paginationSetup.totalArrayPage = totalsArrayPage;
-    //     paginationSetup.previousPage = function () {
-    //         const result = this.PageNumber - 1;
-    //         if(result == 0) return undefined;
-    //         else return result;
-    //     }
-
-    //     paginationSetup.nextPage = function () {
-    //         const result = this.PageNumber - 1;
-    //         if(result > this.totalPage) return undefined;
-    //         else return result;
-    //     }
-
-    //     $('#welcome-call').append(Mustache.render(component,paginationSetup));
-//     })
-// };
