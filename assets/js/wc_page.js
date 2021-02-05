d7c452a441682312c60c7aa98bc040cb80efec81
mly <mly@ongconseil.com>
2021-02-05 10:46:04 +0100

    up Trying to add a pagination

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -16,26 +16,23 @@ $.ajax({
 
         $.get('components/pagination.html', function(templates) {
             var component = $(templates).filter('#pagination-comp').html();
-            console.log(request.getAllResponseHeaders());
-            console.log("pagi: " + request.getResponseHeader("X-Pagination"));
-            // const paginationSetup = JSON.parse(request.getResponseHeader("Content-Type"));
+            const paginationSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
     
-            // console.log(paginationSetup);
-            // const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
-            // paginationSetup.totalArrayPage = totalsArrayPage;
-            // paginationSetup.previousPage = function () {
-            //     const result = this.PageNumber - 1;
-            //     if(result == 0) return undefined;
-            //     else return result;
-            // }
+            const totalsArrayPage = Array.from({length: paginationSetup.totalPage}, (v, i) => i+1);
+            paginationSetup.totalArrayPage = totalsArrayPage;
+            paginationSetup.previousPage = function () {
+                const result = this.PageNumber - 1;
+                if(result == 0) return undefined;
+                else return result;
+            }
     
-            // paginationSetup.nextPage = function () {
-            //     const result = this.PageNumber - 1;
-            //     if(result > this.totalPage) return undefined;
-            //     else return result;
-            // }
+            paginationSetup.nextPage = function () {
+                const result = this.PageNumber - 1;
+                if(result > this.totalPage) return undefined;
+                else return result;
+            }
     
-            // $('#welcome-call').append(Mustache.render(component,paginationSetup));
+            $('#welcome-call').append(Mustache.render(component,paginationSetup));
         });
     }
 })
