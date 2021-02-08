ce137e716cc72268676ec9606638eebf650b8e18
mly <mly@ongconseil.com>
2021-02-08 10:19:05 +0100

    rearrange the code for the WC page. Call the function and ask for a different result depending of the parameter

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -1,42 +1,48 @@
 // get('calls/called/'+cookies.assoId+'?Page=1' , retrieveWelcomeCalls);
 
-$.ajax({
-    url: "https://recette-api.song-fr.com/calls/called/"+cookies.assoId+'?Page=1',
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept":"application/json",
-        "Authorization": cookies.token
-    },
-    method: "GET",
-    success: function (data, textStatus, request) {
-        $.get('components/wc_table.html', function(templates) {
-            var component = $(templates).filter('#tpl-wc-table').html();
-            $('#welcome-call').append(Mustache.render(component,data));
-        });
+if(window.location.hash.includes("#")) {
+    getWCPage(1);
+}
 
-        $.get('components/pagination.html', function(templates) {
-            var component = $(templates).filter('#pagination-comp').html();
-            let paginSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
-            // data.totalArrayPage = Array.from({length: paginSetup.TotalPages}, (v, i) => i+1);
-            $('#welcome-call').append(Mustache.render(component, {
-                "PageSize":paginSetup.PageSize,
-                "PageNumber":paginSetup.PageNumber,
-                "TotalCount":paginSetup.TotalCount,
-                "TotalPages":paginSetup.TotalPages,
-                "nextPage": function () {
-                    const result = this.PageNumber + 1;
-                    if(result > this.totalPage) return undefined;
-                    else return result;
-                },
-                "previousPage": function () {
-                    const result = this.PageNumber - 1;
-                    if(result == 0) return undefined;
-                    else return result;
-                }
-            }));
-        });
-    }
-})
-.fail(function(xhr,textStatus, errorThrown) {
-    getError(xhr,textStatus, errorThrown);
-});
+function getWCPage (data) {
+    $.ajax({
+        url: "https://recette-api.song-fr.com/calls/called/"+cookies.assoId+'?Page='+data,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept":"application/json",
+            "Authorization": cookies.token
+        },
+        method: "GET",
+        success: function (data, textStatus, request) {
+            $.get('components/wc_table.html', function(templates) {
+                var component = $(templates).filter('#tpl-wc-table').html();
+                $('#welcome-call').append(Mustache.render(component,data));
+            });
+    
+            $.get('components/pagination.html', function(templates) {
+                var component = $(templates).filter('#pagination-comp').html();
+                let paginSetup = JSON.parse(request.getResponseHeader("X-Pagination"));
+                // data.totalArrayPage = Array.from({length: paginSetup.TotalPages}, (v, i) => i+1);
+                $('#welcome-call').append(Mustache.render(component, {
+                    "PageSize":paginSetup.PageSize,
+                    "PageNumber":paginSetup.PageNumber,
+                    "TotalCount":paginSetup.TotalCount,
+                    "TotalPages":paginSetup.TotalPages,
+                    "nextPage": function () {
+                        const result = this.PageNumber + 1;
+                        if(result > this.totalPage) return undefined;
+                        else return result;
+                    },
+                    "previousPage": function () {
+                        const result = this.PageNumber - 1;
+                        if(result == 0) return undefined;
+                        else return result;
+                    }
+                }));
+            });
+        }
+    })
+    .fail(function(xhr,textStatus, errorThrown) {
+        getError(xhr,textStatus, errorThrown);
+    });
+}
