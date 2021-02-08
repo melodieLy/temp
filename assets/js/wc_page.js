4c4c520fac68924398fef14dc02f9e2c22c51898
mly <mly@ongconseil.com>
2021-02-08 17:42:23 +0100

    up

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -17,9 +17,7 @@ function getWCPage (data) {
 
             $.get('components/wc_table.html', function(templates) {
                 var component = $(templates).filter('#tpl-wc-table').html();
-                data.forEach(element => {
-                    element.LastContact = moment().format(element.LastContact,'L');
-                });
+                data.LastContact = moment().format(element.LastContact,'L');
                 $('#welcome-call').append(Mustache.render(component,data));
             });
             
