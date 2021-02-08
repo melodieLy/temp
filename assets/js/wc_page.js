b43cf3d68b6ce2b999e6d381e679c5c622681c92
mly <mly@ongconseil.com>
2021-02-08 17:15:56 +0100

    change date. update 404.html

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -17,6 +17,9 @@ function getWCPage (data) {
 
             $.get('components/wc_table.html', function(templates) {
                 var component = $(templates).filter('#tpl-wc-table').html();
+                data.forEach(element => {
+                    element.LastContact = moment().format(element.LastContact,'L');
+                });
                 $('#welcome-call').append(Mustache.render(component,data));
             });
             
