05c6dd143ea467512dfa4c0052e19301452a241c
mly <mly@ongconseil.com>
2021-02-11 10:16:32 +0100

    Up

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -3,8 +3,8 @@ if(!window.location.hash.includes("#")) {
     getWCPage(data, null);
 }
 
-// get("association"+cookies.assoId+"/areas", getWCSearchbar);
-get("association/LNCC/areas", getWCSearchbar);
+get("association"+cookies.assoId+"/areas", getWCSearchbar);
+// get("association/LNCC/areas", getWCSearchbar);
 
 function getWCSearchbar(data) {
     $.get('components/wc_searchbar.html', function(templates) {
