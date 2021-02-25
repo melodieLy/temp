2cba0bb953def6e81fd2fa8b34800994ad7212ea
mly <mly@ongconseil.com>
2021-02-25 17:05:22 +0100

    up

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,7 +74,7 @@ function findAsso(param) {
   })
   .done(function(result) {
     const rights = createCookieAsso(result);
-    if(!rights) alert(xhr.status + "Vous n'avez pas les droits pour accéder aux sites. ");
+    if(!rights) alert("Vous n'avez pas les droits pour accéder au site. ");
     else EnvironmentRedirection();
   })
 
