bf1b9626befc3ab37048c11b36f990281ef8f5c2
mly <mly@ongconseil.com>
2021-03-04 14:50:34 +0100

    up

--- assets/js/base.js
+++ assets/js/base.js
@@ -14,6 +14,24 @@ $.getScript("assets/js/config.js", function () {
             });
             get("context/current-user",callheader);
         });
+
+        $(document).ready(function() {
+            var url = window.location.href;
+            let element = document.getElementsByClassName("has-sub");
+            for (let i = 0; i < element.length; i++) {
+          
+                let navText = element[i].getElementsByTagName('a');
+                for (let j = 0; j < navText.length; j++) {
+                    if(navText[j].href == url) {
+          
+                        element[i].classList.toggle('active');
+                        element[i].classList.toggle('expand');
+                        let t = element[i].getElementsByTagName('ul');
+                        t[0].classList.toggle('show');
+                    }
+                }
+            }
+          });
     }
     else {
         $(function(){
