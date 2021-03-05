c1cabcf79877351608791d0b345586d2ad8d5a38
mly <mly@ongconseil.com>
2021-03-05 11:26:13 +0100

    up

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -12,13 +12,14 @@ function createAssoData(assoName, assoId) {
 function loadSimplySidebarHeader() {
   $.get('components/sidebar_header_simple.html', function(templates) {
   var component = $(templates).filter('#tpl-sidebar-simply').html();
-    $("#association").append(Mustache.render(
-      component,{
-      img_src:retrieveAssoLogo(),
-      Name: cookies.assoName[cookies.actualAsso]
-    }))
+  const rendered = Mustache.render(
+    component,{
+    img_src:retrieveAssoLogo(),
+    Name: cookies.assoName[cookies.actualAsso]
+  });
+    $("#association").html(rendered).promise().done(showSelectedNavElement());
   })
-}
+};
 
 function loadSidebarHeader(){
   let t = createAssoData(cookies.assoName, cookies.assoId);
@@ -48,6 +49,22 @@ function loadSidebarHeader(){
       }
     });
   })
+};
+
+function showSelectedNavElement() {
+  var url = window.location.href;
+  let element = document.getElementsByClassName("has-sub");
+  for (let i = 0; i < element.length; i++) { 
+    let navText = element[i].getElementsByTagName('a');
+    for (let j = 0; j < navText.length; j++) {
+      if(navText[j].href == url) {
+        element[i].classList.toggle('active');
+        element[i].classList.toggle('expand');
+        let t = element[i].getElementsByTagName('ul');
+        t[0].classList.toggle('show');
+      }
+    }
+  }
 };
 
 function changeActualAssociation() {
