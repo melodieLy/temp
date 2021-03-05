653902cec556bc677f9133a9bc7cf8ca6d1d43ec
mly <mly@ongconseil.com>
2021-03-05 12:21:10 +0100

    up

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -85,5 +85,5 @@ function changeAssociationPage(newAsso) {
 }
 
 function retrieveAssoLogo() {
-  return 'https://api.song-fr.com/public/associations/'+cookies.assoId[cookies.actualAsso] + '/logo';
+  return 'https://recette-api.song-fr.com/public/associations/'+cookies.assoId[cookies.actualAsso] + '/logo';
 }
