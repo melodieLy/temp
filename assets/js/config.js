dea7224d21340392ffdfb42a5f1dc4dff56a1c3b
mly <mly@ongconseil.com>
2021-03-16 17:17:41 +0100

    up

--- assets/js/config.js
+++ assets/js/config.js
@@ -18,13 +18,13 @@ $(document).ready(function() {
 function checkValidateCookie() {
   const expiration = new Date(cookies.expires);
   const today = new Date();
-  const link = window.location.hostname;
+  const link = window.location.hostname+ '/temp';
   if(cookies === undefined) {
-      window.location.replace("");
+      window.location.href(link);
       alert("Aucune connexion trouvée. Veuillez-vous authentifier");
   } else if (expiration < today) {
       deleteSession();
-      window.location.replace(link);
+      window.location.href(link);
       showAlert("Connexion expirée. Veuillez-vous reconnecter");
   }
 }
