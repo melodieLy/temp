5cc7579d9fa1e50e13f22b1b7a551e68f8c2946d
mly <mly@ongconseil.com>
2021-04-28 10:51:15 +0200

    up

--- assets/js/dc_page.js
+++ assets/js/dc_page.js
@@ -44,12 +44,15 @@ function getCalendarById(data) {
 
 function checkModifiedData(form) {
     const modified = form.getElementsByTagName("input");
-    var data = {};
-    data.Id = modified.id.value;
-    data.startDate = modified.startDate.value;
-    data.endDate = modified.endDate.value;
-    data.debitDate = modified.debitDate.value;
-    console.log(data);
+    // var data = {};
+    // data.Id = modified.id.value;
+    // data.startDate = modified.startDate.value;
+    // data.endDate = modified.endDate.value;
+    // data.debitDate = modified.debitDate.value;
+    // console.log(data);
+
+    var data = "Id="+modified.id.value+"&startDate="+modified.startDate.value+"&endDate="+modified.endDate.value+"&debitDate="+modified.debitDate.value;
+
 
     put("associations/" + cookies.assoId[cookies.actualAsso] + "/debitCalendar", data);
 }
