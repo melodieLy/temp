44a63c2b14ec422eaa68054b37864644fea503a7
mly <mly@ongconseil.com>
2021-06-01 10:56:42 +0200

    up

--- assets/js/wc_page.js
+++ assets/js/wc_page.js
@@ -34,10 +34,12 @@ function getWCSearchbar(data) {
  
 function getWCPage (data,param) {
     //Update the sessionStorage if only the "RNR" was send
-    if (typeof (param) == String) sessionStorage.setItem("rnr", param);
-    do {
+    if (typeof (param) == String) {
         sessionStorage.setItem("rnr", param);
-    } while (sessionStorage.getItem("rnr") != param);
+        do {
+            sessionStorage.setItem("rnr", param);
+        } while (sessionStorage.getItem("rnr") != param);
+    }
     const urlParam = getUrlParam(param);
     try {
         getWelcomeCall(data + urlParam);
